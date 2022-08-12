8780167031fbb64d2169f0e53affffa998c31d58
8099751086 <dilwarhussainit24@gmail.com>
2022-08-13 00:19:36 +0530

    Morethan Three Parameters

--- Genericsmain.java
+++ Genericsmain.java
@@ -1,17 +1,17 @@
 package com.generics;
 
-
 public class Genericsmain {
-	
-	    public static void main(String[] args) {
-	     System.out.println("welcome to Generics Program");
-	     MaxValue<Integer> maxValue = new MaxValue<>(5,4,3);
-	        maxValue.testMaximum();
-	        MaxValue<Float> maxValue1 = new MaxValue<>(5.2F,8.3F,3.2F);
-	        maxValue1.testMaximum();
-	        MaxValue<String> maxValue2 = new MaxValue<>("Peach","Apple","Zach");
-	        maxValue2.testMaximum();
-	    }
+
+	public static void main(String[] args) {
+		System.out.println("welcome to Generics Program");
+		Integer[] intArray = { 6, 8, 5, 7 };
+		Float[] floatArray = { 6.3F, 7.2F, 3.4F, 5.2F };
+		String[] stringArray = { "vipul", "ajay", "sam", "anusha" };
+		MaxValue<Integer> maxValue = new MaxValue<>(intArray);
+		maxValue.testMaximum();
+		MaxValue<Float> maxValue1 = new MaxValue<>(floatArray);
+		maxValue1.testMaximum();
+		MaxValue<String> maxValue2 = new MaxValue<>(stringArray);
+		maxValue2.testMaximum();
 	}
-
-
+}
