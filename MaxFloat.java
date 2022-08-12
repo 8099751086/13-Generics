d7b991b7837dd7ea2e57eba1b1cf4474b8a13436
8099751086 <dilwarhussainit24@gmail.com>
2022-08-12 23:39:03 +0530

    Max Float

--- MaxFloat.java
+++ MaxFloat.java
@@ -1,8 +1,7 @@
 package com.generics;
 
-public class MaxInteger {
-
-	void maxvalue(Integer val1, Integer val2, Integer val3) {
+public class MaxFloat {
+	void maxvalue(Float val1, Float val2, Float val3) {
 		if ((val1.compareTo(val2)) >= 0 && (val1.compareTo(val3)) >= 0)
 			System.out.println(val1 + " is the largest");
 		else if ((val2.compareTo(val1)) >= 0 && (val2.compareTo(val3)) >= 0) {
@@ -11,5 +10,4 @@ public class MaxInteger {
 			System.out.println(val3 + " is largest");
 		}
 	}
-
 }
